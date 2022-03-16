30d85ab47230cd01c0d605be341fa1da7317747c
furkanmemis <77626136+furkanmemis@users.noreply.github.com>
2022-03-16 12:05:38 +0300

    Add files via upload

---
+++ CokluInterface/CokluInterface/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CokluInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci ogrenci1 = new ogrenci("furkan","memiş",20,"16-in","erkek",401,90);
+            ogrenci[] dizi = new ogrenci[5];
+            {
+                Console.WriteLine("Ogrenci");
+                Console.WriteLine("-----");
+
+                ogrenci1.yemek();
+                Console.WriteLine("-----");
+
+                ogrenci1.kos();
+                Console.WriteLine("-----");
+
+                ogrenci1.calis();
+                Console.WriteLine("-----");
+
+                ogrenci1.bilgi();
+                Console.WriteLine("-----");
+
+                ogrenci1.sosyal();
+                Console.WriteLine("-----");
+                Console.WriteLine("-----");
+                Console.WriteLine("-----");
+
+
+            }
+            cita cita1 = new cita("alex","stark",13,"143-3","erkek",5,"afrika");
+            cita[] dizi1 = new cita[4];
+            {
+                Console.WriteLine("Cita");
+                Console.WriteLine("-----");
+
+                cita1.avlan();
+                Console.WriteLine("-----");
+
+                cita1.saldir();
+                Console.WriteLine("-----");
+
+                cita1.kac();
+                Console.WriteLine("-----");
+
+                cita1.bilgi();
+                Console.WriteLine("-----");
+
+            }
+
+            foreach(var k in dizi)
+            {
+                Console.WriteLine(k);
+            }
+            foreach(var k in dizi1)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CokluInterface/CokluInterface/ogrenci.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CokluInterface
+{
+    internal class ogrenci:insan,Interface
+    {
+        private int okul_no { get; set; }
+        private int not { get; set; }
+
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public int yas { get; set; }
+        public string ıd { get; set; }
+        public string cinsiyet { get; set; }
+
+        public ogrenci(string isim,string soyisim,int yas,string ıd,string cinsiyet,int okul_no,int not)
+        {
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.yas = yas;
+            this.ıd = ıd;
+            this.cinsiyet = cinsiyet;
+            this.okul_no = okul_no;
+            this.not = not;
+
+        }
+        public void yemek()
+        {
+            base.yemek();
+        }
+        public void kos()
+        {
+            base.kos();
+        }
+        public void calis()
+        {
+            base.calis();
+        }
+
+        public void bilgi()
+        {
+            Console.WriteLine("isim: {0}\nsoy isim: {1}\nyas: {2}\nıd: {3}\ncinsiyet:{4}", isim, soyisim, yas, ıd, cinsiyet);
+            Console.WriteLine("okul no: {0}\nnot: {1}", okul_no, not);
+        }
+        public void sosyal()
+        {
+            kulüp.etkinlik();
+        }
+        
+        
+    }
+}
